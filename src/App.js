c86a6ac7af9fb1cc0098105821d9f5ac29ad9856
yahya-1 <y.a.m.k123456@hotmail.com>
2022-10-03 18:34:55 +0300

    Create a section for About Component and add its style. relate #10

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import About from './components/about/About';
 import Header from './components/header/Header';
 import Home from './components/home/Home';
 
@@ -7,9 +8,10 @@ function App() {
   return (
     <>
       <Header />
-      
+
       <main className='main'>
-        <Home/>
+        <Home />
+        <About />
       </main>
     </>
   );
