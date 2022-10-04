b1b3ff869a2c605921fee4363b5271a44104a06e
yahya-1 <y.a.m.k123456@hotmail.com>
2022-10-04 20:07:22 +0300

    Add a custom scroll to a page and it works to move to the home section with the shadow order of the header with scroll operation. relate #24

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
 import Home from './components/home/Home';
 import Qualification from './components/qualification/Qualification';
+import ScrollUp from './components/scrollup/ScrollUp';
 import Services from './components/services/Services';
 import Skills from './components/skills/Skills';
 
@@ -21,8 +22,9 @@ function App() {
         <Services />
         <Qualification />
         <Contact />
-        <Footer />
       </main>
+      <Footer />
+      <ScrollUp/>
     </>
   );
 }
