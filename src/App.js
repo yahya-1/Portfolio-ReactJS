94341bb1bc60217c8c5c91093c0e5e7acfe83c61
yahya-1 <y.a.m.k123456@hotmail.com>
2022-10-04 15:05:41 +0300

    Create a section for the Contact Component section and its style. relate #18

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import About from './components/about/About';
+import Contact from './components/contact/Contact';
 import Header from './components/header/Header';
 import Home from './components/home/Home';
 import Qualification from './components/qualification/Qualification';
@@ -18,6 +19,7 @@ function App() {
         <Skills />
         <Services />
         <Qualification />
+        <Contact />
       </main>
     </>
   );
